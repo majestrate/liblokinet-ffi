f6c3ed2fd6290d38aeff7505a5b8f595d4d8d1fb
Jeff Becker <jeff@i2p.rocks>
2021-09-21 13:41:39 -0400

    make nice wrapper for liblokinet

--- node/lokinet.js
+++ node/lokinet.js
@@ -6,58 +6,124 @@ import fetch from 'node-fetch';
 import { readFile } from 'fs/promises';
 
 import { connect } from 'net';
-
+import { Agent, get } from 'http';
+import { inherits } from 'util';
+import { EventEmitter } from 'events';
 
 const lokinet = bindings('liblokinet_js');
 
-const make_buffer = (str) => {
-  let arr = Array.from(str, (e) => e.charCodeAt(0) );
-  return new Uint8Array(arr).buffer;
+export class Lokinet
+{
+
+  /// @brief construct a lokinet
+  /// @params opts can be null or a dict with the keys `bootstrap` which points to the bootstrap.signed file
+  constructor(opts)
+  {
+    this._opts = opts || {};
+    this._ctx = null;
+  }
+
+  /// @brief start lokinet
+  async start()
+  {
+    if(this._ctx)
+    {
+      this.stop();
+    }
+    this._ctx = new lokinet.Context();
+
+    const bootstrap = this._opts.bootstrap || "bootstrap.signed";
+    const data = await readFile(bootstrap);
+    const rc = new Uint8Array(data).buffer;
+    this._ctx.bootstrap(rc);
+    this._ctx.start();
+  }
+
+  /// @brief stop lokinet
+  stop()
+  {
+    this._ctx.stop();
+    this._ctx = null;
+  }
+
+  /// @brief get our .loki address
+  get hostname()
+  {
+    return this._ctx.addr();
+  }
+
+  /// @brief connect to host:port with a connect callback
+  connect(port, host, callback)
+  {
+    const stream_info = this._ctx.outbound_stream(`${host}:${port}`);
+    const conn = connect(stream_info.port, stream_info.host, callback);
+    conn.on("error", (err) => {
+      console.log(err);
+      conn.end();
+    });
+    conn.on('end', () => {
+      if(this._ctx)
+      {
+        this._ctx.close_stream(stream_info.id);
+      }
+    });
+    return conn;
+  }
+
+  /// @brief make an http.Agent that uses lokinet
+  agent(options)
+  {
+    return new _Agent(this, options);
+  }
+
+};
+
+class _Agent extends Agent
+{
+  constructor(lokinet, options)
+  {
+    super(options);
+    this._ctx = lokinet;
+  }
+
+  createConnection(options, callback)
+  {
+    const conn = this._ctx.connect(options.port || 80, options.host, () => {
+      if(callback)
+      {
+        callback(null, conn);
+      }
+    });
+    return conn;
+  }
 };
 
 const runit = async () => {
 
-  let ctx = new lokinet.Context();
+  let ctx = new Lokinet();
 
-  console.log("getting bootstrap...");
-  // const resp = await fetch('https://seed.lokinet.org/lokinet.signed');
-  // const rc = make_buffer(await resp.text());
-  const data = await readFile('bootstrap.signed');
-  const rc = new Uint8Array(data).buffer;
+  await ctx.start();
+  console.log(`we are ${ctx.hostname}`);
 
-  ctx.bootstrap(rc);
-  console.log("bootstrapped");
-  ctx.start();
-  console.log("Started as " + ctx.addr());
+  // make an http agent
+  const agent = ctx.agent();
 
-  const stream_info = ctx.outbound_stream("dw68y1xhptqbhcm5s8aaaip6dbopykagig5q5u1za4c7pzxto77y.loki:80");
-  console.log(`got stream info: ${stream_info.host}:${stream_info.port}`);
-
-
-  const end = () => {
-    ctx.close_stream(stream_info.id);
-    ctx.stop();
-    console.log("stopped");
-  };
-
-
-  const conn = connect(stream_info.port, stream_info.host, () => {
-    console.log("sending request");
-    conn.write('GET / HTTP/1.1\r\nHost: probably.loki\r\n\r\n');
+  // do a get request
+  const req = get({
+    hostname: "dw68y1xhptqbhcm5s8aaaip6dbopykagig5q5u1za4c7pzxto77y.loki",
+    path: "/",
+    agent: agent
   });
-  conn.on('data', (data) => {
-    console.log(data.toString());
-    conn.end();
+  req.on('response', (resp) => {
+    resp.on("data", (data) => {
+      console.log("got data");
+      console.log(data.toString());
+    });
+    resp.on("end", () => {
+      ctx.stop();
+    });
   });
-  conn.on('error', (err) => {
-    console.log(`connection error: ${err}`);
-    conn.end();
-  });
-  conn.on('end', () => {
-    end();
-  });
-
+  req.end();
 };
 
-
-await runit();
+// await runit();
