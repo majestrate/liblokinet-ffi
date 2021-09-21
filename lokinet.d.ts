279b050ba3e115e7e5159c333fc0c153c4feb254
Jeff Becker <jeff@i2p.rocks>
2021-09-21 17:47:10 -0400

    typescript jizz

---
+++ lokinet.d.ts
@@ -0,0 +1,14 @@
+
+
+export default class Lokinet
+{
+    constructor(opts: any);
+    localip() : Promise<string>;
+    start(): Promise<undefined>;
+    stop(): void;
+    hostname(): Promise<string>;
+    connect(port: Number, host: string, callback: any): any;
+    agent(options: any): any;
+}
+
+export function hex_to_base32z(hex: string): string;
